73d329cbb66e96cc8e0db1f90ee939f93dcdcd54
Shourjya Chakraborty <137634522+sc-likes-to-code@users.noreply.github.com>
2025-04-25 13:18:21 +0530

    Add files via upload

---
+++ ass1_3.pl
@@ -0,0 +1,10 @@
+% Ownership Facts
+owns(jack, car(bmw)).
+owns(john, car(chevy)).
+owns(olivia, car(civic)).
+owns(jane, car(chevy)).
+
+% Car Type Facts
+sedan(car(bmw)).
+sedan(car(civic)).
+truck(car(chevy)).
